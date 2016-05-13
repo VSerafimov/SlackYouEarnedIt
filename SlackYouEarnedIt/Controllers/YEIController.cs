36dc95f1819123eb90efc9006e38f55cdb5b9e51
Vladimir Serafimov <vladimir@clear-measure.com>
2016-05-13 14:42:02 -0400

    Created YEIController post.

---
+++ SlackYouEarnedIt/Controllers/YEIController.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace SlackYouEarnedIt.Controllers
+{
+    public class YEIController : ApiController
+    {
+        [HttpPost]
+        [ResponseType(typeof(string))]
+        public HttpResponseMessage Post()
+        {
+            string data = Request.Content.ReadAsStringAsync().Result;
+
+            HttpResponseMessage httpResponseMessage = new HttpResponseMessage() { Content = new ObjectContent(typeof(string), data, new JsonMediaTypeFormatter()) };
+
+            return httpResponseMessage;
+        }
+    }
+}
